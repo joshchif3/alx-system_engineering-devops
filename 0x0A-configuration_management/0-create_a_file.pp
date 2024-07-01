3dbd7e8f5574c1a74c514152d0f76d3ad602fcd4
joshchif3 <pantherchif3@gmail.com>
2024-07-01 10:06:02 -0700

    my file changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creating file in the tmp
+# creates a file in /tmp
 
 file { '/tmp/school':
-    content => 'I love Puppet',
-    mode    => '0744';
-    owner   => 'www-data',
-    group   => 'www-data'.
-   }
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
