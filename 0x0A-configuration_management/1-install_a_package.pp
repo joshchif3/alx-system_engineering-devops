96bac166cbbdc97fdd90ab2e18552fa47570b30a
joshchif3 <pantherchif3@gmail.com>
2024-07-01 10:33:05 -0700

    my files added

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-# 1-install_a_package.pp
-package { 'flask':
+#Using Puppet, install flask from pip3
+
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider =>'pip3',
+}
+package { 'Flask':
   ensure   => '2.1.0',
+  provider =>'pip3',
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
+  require  => Package'[Flask]',
 }
